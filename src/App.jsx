cb91010540588c2c3ff704192681370d8d094b87
unknown <finnneron1@gmail.com>
2022-02-28 20:05:02 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,17 @@ function App() {
                 <button onClick={()=>{switchTheme('whiteblue'); window.scrollTo(0, 0);}}></button>
                 <DarkModeOutlinedIcon onClick={()=>{switchTheme('dark'); window.scrollTo(0, 0);}} className="DarkModeOutlinedIcon" />
             </div>
+            <CookieConsent
+            disableStyles={true}
+            location="bottom"
+            buttonText="Okay"
+            cookieName="denniskibet"
+            buttonClasses="button"
+            containerClasses="cookieConsent"
+            expires={150}
+            >
+                <p className="cookieText">We use cookies to improve your browsing experience. And <span className="cookiehiglight">Pizzas</span> to boost morale on Fridays. <span className="cookiehiglight">Learn about our <Link to="/privacy" className="privacyPolicy">Privacy policy</Link> here.{" "}</span></p>
+            </CookieConsent>
             <Landing />
         </div>
     );
