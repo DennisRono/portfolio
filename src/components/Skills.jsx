e1a305e456e120d6c4ed108929f8f4a270e5d6a2
unknown <finnneron1@gmail.com>
2022-03-02 11:01:03 +0300

    simply war thought attempt shadow

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,16 @@ import '../styles/css/skills.css'
 const Skills = () => {
   return (
     <div className="skills">
-        
+        <div className="swrapper">
+            <div className="scontainer">
+                <div className="sflex">
+                    <div className="sTexts"></div>
+                    <div className="sImages">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
