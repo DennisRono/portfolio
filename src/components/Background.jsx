582da27c77b38e80470c9575619a5bad7e7b4c46
unknown <finnneron1@gmail.com>
2022-03-02 10:35:03 +0300

    date spent graph rush neck

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -16,7 +16,7 @@ const background = () => {
                         </div>
                     </div>
                     <div className="bTexts">
-                        <h2 className="btTitle"><span className="secCounter">1.</span> Background <span className="line"></span></h2>
+                        <h2 className="btTitle"><span className="secCounter">1.</span> &nbsp; Background <span className="line"></span></h2>
                         <p className="backtexts">
                         Hello, I am Dennis. I am currently a student at Machackos University Kenya. I love building things for the internet. My native home place is Kericho Kenya. My intrest in computers started back when I was in High School when I did a science fair project on web develpment. As simple as the project was it taught me alot on how the internet works and moreover gave me a little Experience on HTML & CSS.
                         <br /><br />
