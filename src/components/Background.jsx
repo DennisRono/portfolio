0eec122ed59edbc5561c14c91753e1e3da33d164
unknown <finnneron1@gmail.com>
2022-03-02 09:55:05 +0300

    steel driven poor dog larger

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,7 +7,13 @@ const background = () => {
         <div className="bwrapper">
             <div className="bcontainer">
                 <div className="bflex">
-                    
+                    <div className="dennisImage"></div>
+                    <div className="bTexts">
+                        <h2><span className="secCounter">1.</span> Background</h2>
+                        <p className="backtexts">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ullam inventore quasi ex eius eaque cupiditate expedita velit officia ut, ab quo quibusdam eum esse ad dolores molestiae vitae tenetur, facilis voluptas magni nihil modi exercitationem. Consectetur reprehenderit soluta, tenetur dolorum officiis velit quis laboriosam magnam pariatur laudantium, mollitia atque?
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
