1ee5696923573d15ab5db25dde84cd79a407200e
unknown <finnneron1@gmail.com>
2022-03-02 09:56:04 +0300

    rising shop unhappy light dead

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -9,7 +9,7 @@ const background = () => {
                 <div className="bflex">
                     <div className="dennisImage"></div>
                     <div className="bTexts">
-                        <h2><span className="secCounter">1.</span> Background</h2>
+                        <h2><span className="secCounter">1.</span> Background <span className="line"></span></h2>
                         <p className="backtexts">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ullam inventore quasi ex eius eaque cupiditate expedita velit officia ut, ab quo quibusdam eum esse ad dolores molestiae vitae tenetur, facilis voluptas magni nihil modi exercitationem. Consectetur reprehenderit soluta, tenetur dolorum officiis velit quis laboriosam magnam pariatur laudantium, mollitia atque?
                         </p>
