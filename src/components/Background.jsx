813f42030d464aff3971ad1ef2f5ba549d2ba888
unknown <finnneron1@gmail.com>
2022-03-02 10:12:08 +0300

    help powerful bone move record

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -16,7 +16,7 @@ const background = () => {
                         </div>
                     </div>
                     <div className="bTexts">
-                        <h2><span className="secCounter">1.</span> Background <span className="line"></span></h2>
+                        <h2 className="btTitle"><span className="secCounter">1.</span> Background <span className="line"></span></h2>
                         <p className="backtexts">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ullam inventore quasi ex eius eaque cupiditate expedita velit officia ut, ab quo quibusdam eum esse ad dolores molestiae vitae tenetur, facilis voluptas magni nihil modi exercitationem. Consectetur reprehenderit soluta, tenetur dolorum officiis velit quis laboriosam magnam pariatur laudantium, mollitia atque?
                         </p>
