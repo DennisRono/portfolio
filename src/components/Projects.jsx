e338c1f1f9dcec172d1b61665895f510737b6b69
unknown <finnneron1@gmail.com>
2022-03-03 21:21:01 +0300

    he effect pan grandfather move

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,9 +7,12 @@ const Projects = () => {
             <div className="pwrapper">
                 <div className="pcontainer">
                     <h2 className="pTitle"><span className="pCounter">04. </span> &nbsp;&nbsp;&nbsp;&nbsp; Featured Projects</h2>
-                    <div className="pflex">
-                        <div className="projImage"></div>
-                        <div className="projTexts"></div>
+                    <div className="proj">
+                        <h2 className="projTitle">LinkedIn clone</h2>
+                        <div className="pflex">
+                            <div className="projImage"></div>
+                            <div className="projTexts"></div>
+                        </div>
                     </div>
                 </div>
             </div>
