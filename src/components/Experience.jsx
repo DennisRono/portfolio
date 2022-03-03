70806863bd70ee9a1bb4b0ef5b89165ca0fa51b8
unknown <finnneron1@gmail.com>
2022-03-03 20:04:01 +0300

    during research made trade lift

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,14 +11,21 @@ const Experience = () => {
                         <h2 className="eTitle"><span className="eCounter">03. </span> &nbsp;&nbsp;&nbsp;&nbsp; Experience & Schooling</h2>
                         <div className="experiences">
                             <div className="exp">
-                                <h2 className="eOneTitle">Samoei Boys Secondary School</h2>
+                                <h2 className="eOneTitle">Samoei Boys Secondary School, Nandi Hills</h2>
                                 <div className="eOneTexts">
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum velit commodi possimus hic cum inventore labore sed doloremque impedit, distinctio tempora mollitia voluptatem vitae, excepturi reiciendis. Dolor reiciendis laudantium vero?</p>
                                     <span className="eduration">2016 Jan - 2019 Dec</span>
                                 </div>
                             </div>
                             <div className="exp">
-                                <h2 className="eOneTitle">Machackos University</h2>
+                                <h2 className="eOneTitle">Alphax College, Eldoret</h2>
+                                <div className="eOneTexts">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum velit commodi possimus hic cum inventore labore sed doloremque impedit, distinctio tempora mollitia voluptatem vitae, excepturi reiciendis. Dolor reiciendis laudantium vero?</p>
+                                    <span className="eduration">2016 Jan - 2019 Dec</span>
+                                </div>
+                            </div>
+                            <div className="exp">
+                                <h2 className="eOneTitle">Machackos University, Machackos</h2>
                                 <div className="eOneTexts">
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum velit commodi possimus hic cum inventore labore sed doloremque impedit, distinctio tempora mollitia voluptatem vitae, excepturi reiciendis. Dolor reiciendis laudantium vero?</p>
                                     <span className="eduration">2020 Jan - current</span>
