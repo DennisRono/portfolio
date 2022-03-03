cd50e01f1499b6a2d1920d51c43e7fd328da25a9
unknown <finnneron1@gmail.com>
2022-03-03 18:32:03 +0300

    period none cut shells entire

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/css/projects.css'
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export default Projects
