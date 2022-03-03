2caef7098c800f8b8bc6576d7d5c4bd15a805fe5
unknown <finnneron1@gmail.com>
2022-03-03 18:13:11 +0300

    result village wool guide continued

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,9 +8,9 @@ const Experience = () => {
             <div className="econtainer">
                 <div className="eflex">
                     <div className="eTexts">
-                        <h2 className="eTitle"><span className="eCounter">3. </span> &nbsp;&nbsp; Experience</h2>
+                        <h2 className="eTitle"><span className="eCounter">3. </span> &nbsp;&nbsp; Experience & Schooling</h2>
                         <div className="experiences">
-                            <div className="expOne">
+                            <div className="exp">
                                 <h2 className="eOneTitle">Samoei Boys Secondary School</h2>
                                 <div className="eOneTexts">
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum velit commodi possimus hic cum inventore labore sed doloremque impedit, distinctio tempora mollitia voluptatem vitae, excepturi reiciendis. Dolor reiciendis laudantium vero?</p>
