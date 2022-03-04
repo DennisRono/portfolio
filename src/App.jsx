b7be2ce06c3450cc045a2e7a57805a7afda6317a
unknown <finnneron1@gmail.com>
2022-03-04 17:18:03 +0300

    walk nature excited could shirt

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Background from './components/Background'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
     const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -63,6 +64,7 @@ function App() {
                 <Skills />
                 <Experience />
                 <Projects />
+                <Contact />
             </div>
         </div>
     );
