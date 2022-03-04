1818cb2b8a3762b43bb01b660ed06d671acaf9ea
unknown <finnneron1@gmail.com>
2022-03-04 17:20:03 +0300

    long powder union wife into

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,13 @@ import '../styles/css/contact.css'
 const Contact = () => {
   return (
     <div className="contact">
-        
+        <div className="conwrapper">
+            <div className="concontainer">
+                <div className="conflex">
+                <h2 className="conTitle"><span className="conCounter">03. </span> &nbsp;&nbsp;&nbsp;&nbsp; what are you upto?</h2>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
