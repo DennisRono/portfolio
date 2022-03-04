4f42a8f241c1e74b2bb47b342c350b3591e22456
unknown <finnneron1@gmail.com>
2022-03-04 17:27:06 +0300

    experience unless sudden appearance could

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,9 @@ const Contact = () => {
                 <div className="conflex">
                     <h2 className="conTitle"><span className="conCounter">03. </span> &nbsp;&nbsp;&nbsp;&nbsp; what are you upto?</h2>
                     <h2 className="constitle">Let's talk</h2>
+                    <p className="contexts">
+                        let's chat on opportunities, new technologies, ideas or even say hi
+                    </p>
                 </div>
             </div>
         </div>
