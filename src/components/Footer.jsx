b424d575a5be0b6a25137d3f672c59a0676c7485
unknown <finnneron1@gmail.com>
2022-03-04 17:57:03 +0300

    measure earn locate pink chemical

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../styles/css/footer.css'
+import { FiGithub } from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+        <div className="fwrapper">
+            <div className="fcontainer">
+                <div className="fflex">
+                    <div className="fcopy">
+                        <p>
+                            &copy; denniskibet {getCurrentYear} | <span><a href="https://github.com/DennisRono/portfolio"><FiGithub title="Github"/></a></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
