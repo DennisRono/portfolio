5374e36b864fafae3b570cc0e43e8a17a71694df
DennisRono <finnneron1@gmail.com>
2022-03-09 17:21:07 +0300

    smooth pictured had yesterday yourself

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Landing from './components/Landing'
-import Background from './components/Background'
-import Skills from './components/Skills'
-import Experience from './components/Experience'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Landing from '../components/Landing'
+import Background from '../components/Background'
+import Skills from '../components/Skills'
+import Experience from '../components/Experience'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
