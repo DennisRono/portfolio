9b957d4bdc7755d5cb530ef63f76e1ba18aed18e
DennisRono <finnneron1@gmail.com>
2022-03-09 17:35:01 +0300

    recall amount nest price bean

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/css/notfound.css'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
+    const {initialSeconds = 10} = props;
+    const [seconds, setSeconds ] =  useState(initialSeconds);
+    useEffect(()=>{
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            } else {
+                window.location.href = "/"
+            }
+        }, 1000)
+        return ()=> {
+            clearInterval(myInterval);
+        };
+    });
   return (
     <div>
-        <p>404</p>
+        <div className="notfoundWrapper">
+            <p>404</p>
+            <br/>
+            <p className="notFoundTexts">We can't seem to find the page you are looking for.<br /> We will try to redirect you automatically in { seconds } seconds.</p>
+            <Link className="notFoundLink" to="/">back home</Link>             
+        </div>
     </div>
   )
 }
