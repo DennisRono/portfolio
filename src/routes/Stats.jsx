a8f1fa1cefdef660390e49824f7de3c65d45a624
DennisRono <finnneron1@gmail.com>
2022-03-10 09:05:02 +0300

    partly inch take everything neighborhood

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -14,6 +14,12 @@ const Stats = () => {
                   <img alt="Finn Neron's Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=DennisRono&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true" />
               </a>
               </div>
+              <div className="sactivity">
+                <a href="https://denniskibet.com/">
+                <img height="137px" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
+                <img height="137px" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
