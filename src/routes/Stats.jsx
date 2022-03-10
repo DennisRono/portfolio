927265060d43ffd7a0a8e2467eaabf05a19a81ea
DennisRono <finnneron1@gmail.com>
2022-03-10 14:32:01 +0300

    near together whispered fur gold

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -54,6 +54,12 @@ const Stats = () => {
                   </div>
                 </div>
               </div>
+              <div className="sactivity">
+                <a href="https://denniskibet.com/">
+                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
+                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
+                </a>
+              </div>
               <div className="contributionGraph">
                 <div id="contrGraph"></div>
               </div>
@@ -61,12 +67,6 @@ const Stats = () => {
               <a align ="center" href="https://denniskibet.com">
                   <img alt="Finn Neron's Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=DennisRono&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true" />
               </a>
-              </div>
-              <div className="sactivity">
-                <a href="https://denniskibet.com/">
-                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
-                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
-                </a>
               </div>
             </div>
           </div>
