0cb7a3245724abdf22b04d03105dc82ee6302eff
DennisRono <finnneron1@gmail.com>
2022-03-10 10:44:03 +0300

    where funny unhappy song giant

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 import '../styles/css/stats.css'
 import axios from 'axios'
 import BackImage from '../images/Linkedin.png'
-import { Chart, ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Decimation, Filler, Legend, Title, Tooltip, SubTitle} from 'chart.js'
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
 import {actions, config} from '../includes/chart'
 
 
 
 const Stats = () => {
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+      onClick: (e) => {
+        const canvasPosition = getRelativePosition(e, chart);
   
+        // Substitute the appropriate scale IDs
+        const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+        const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+      }
+    }
+  });
   return (
     <div>
       <div className="stats">
