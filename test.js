9f71b3de76379cec6cf154fff7b3cbf93f29244a
DennisRono <finnneron1@gmail.com>
2022-03-10 10:08:01 +0300

    shape diameter piano soldier worry

--- test.js
+++ test.js
@@ -2,14 +2,14 @@
 var GhPolyglot = require("gh-polyglot");
 
 // Stats about git-stats
-var me = new GhPolyglot("IonicaBizau/git-stats");
+var me = new GhPolyglot("DennisRono");
 
 // Repository stats
-me.repoStats(function (err, stats) {
-    console.log(err || stats);
-});
-
-// User stats
-// me.userStats(function (err, stats) {
+// me.repoStats(function (err, stats) {
 //     console.log(err || stats);
 // });
+
+// User stats
+me.userStats(function (err, stats) {
+    console.log(err || stats);
+});
