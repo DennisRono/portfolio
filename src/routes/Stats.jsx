b88f52de8aba175c9ae7f112a42b269b526b0cde
DennisRono <finnneron1@gmail.com>
2022-03-10 10:46:06 +0300

    were pattern spent vast sharp

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -9,6 +9,25 @@ import {actions, config} from '../includes/chart'
 
 
 const Stats = () => {
+  const ctx = document.getElementById('contrGraph');
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
   const chart = new Chart(ctx, {
     type: 'line',
     data: data,
@@ -57,7 +76,7 @@ const Stats = () => {
                 </div>
               </div>
               <div className="contributionGraph">
-                <Line data={contrData} options={config} />
+                <div id="contrGraph"></div>
               </div>
               <div className="dennisGraph">
               <a align ="center" href="https://denniskibet.com">
