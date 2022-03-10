d4a3e72b3fc7e86b115bc80501cce57a6d11ee81
DennisRono <finnneron1@gmail.com>
2022-03-10 15:14:03 +0300

    who extra safe principal steel

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -54,7 +54,7 @@ const Stats = () => {
                   </div>
                 </div>
               </div>
-              <div className="sactivity">
+              <div className="sactivity" style={{margin:"0 auto"}}>
                 <a href="https://denniskibet.com/">
                 <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
                 <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
