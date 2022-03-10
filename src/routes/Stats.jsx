2a8919bbc7873fa689414d756104daff980c7196
DennisRono <finnneron1@gmail.com>
2022-03-10 09:10:02 +0300

    face fourth basis record soft

--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -9,6 +9,9 @@ const Stats = () => {
         <div className="statsWrapper">
           <div className="statsContainer">
             <div className="statsFlex">
+              <div className="sprofile">
+
+              </div>
               <div className="dennisGraph">
               <a align ="center" href="https://denniskibet.com">
                   <img alt="Finn Neron's Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=DennisRono&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true" />
@@ -16,8 +19,8 @@ const Stats = () => {
               </div>
               <div className="sactivity">
                 <a href="https://denniskibet.com/">
-                <img height="137px" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
-                <img height="137px" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
+                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api?username=DennisRono&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&theme=dark" />&nbsp;
+                <img height="137px" alt="" src="https://github-readme-stats.vercel.app/api/top-langs/?username=DennisRono&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=6&exclude_repo=comp426,Redventures-Movie-Quotes&theme=dark" />
                 </a>
               </div>
             </div>
