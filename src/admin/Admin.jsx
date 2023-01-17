7e17e91dd0a4dd5f09fcd6a769dd8a526f0b8ee9
DennisRono <finnneron1@gmail.com>
2023-01-17 18:28:01 +0300

    friendly stomach television engine saved

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,11 +1,21 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import '../styles/css/admin.css'
+import { useNavigate } from "react-router-dom";
+
 
 const Admin = () => {
+    let navigate = useNavigate();
+    let LoggedIn = false
+
+    useEffect(() => {
+        if (LoggedIn){
+           return navigate("/");
+        }
+     },[LoggedIn, navigate]);
   return (
     <Fragment>
         <div className="admin">
-            
+            <h3>admin</h3>
         </div>
     </Fragment>
   )
