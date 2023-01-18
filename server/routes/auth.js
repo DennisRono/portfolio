4c1f791e56d1d30b05f417a55383a4af4ffa8e77
DennisRono <finnneron1@gmail.com>
2023-01-18 07:42:02 +0300

    truth excellent were writer increase

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 //login a user to the system
 router.post("/login", async (req, res, next) => {
-    
+    req = req.body
+    res.json(req)
 })
 
 module.exports = router;
