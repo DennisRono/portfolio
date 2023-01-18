8d0079e76f60bc6610b438b6be4818caa893fba1
DennisRono <finnneron1@gmail.com>
2023-01-18 08:22:00 +0300

    practical tight balance late motion

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,37 @@ const bcrypt = require('bcrypt')
 const db = require("../utils/auth")
 const { loginDataSchema } = require("../schemas/user")
 
+//register a new user in the system
+router.post("/register", async (req, res, next) => {
+    try{
+        const validate = req.body
+        await db.one(validate.email).then((user) => {
+            if (user) {return res.json({"status": 400,"type":"Error","message":"user is already registered!"});}
+            else{
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) {return res.status(422).send(err.message);}
+                    bcrypt.hash(validate.password, salt, (err, hash) => {
+                        if (err) {return res.status(422).send(err.message);}
+                        let userid = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2)
+                        const timestamp = moment().tz("Africa/Nairobi").format('YYYY-MM-DD HH:mm')
+                        try {
+                            let uobj = { username: validate.username, email: validate.email, password: hash, userID: userid, timestamp: timestamp }
+                            db.create(uobj);
+                            res.json({status: 200, type: "success", message: "registered successfully!"})
+                        } catch (err) { res.json({"status": 500,"type":"Error","details":err}); }
+                    })
+                })
+            }
+        })
+    } catch (err){
+        if (err.isJoi === true) {
+            res.json({"status": 400,"type":"Error","message":err.details[0].message})
+        } else {
+            res.json({"status": 500,"type":"Error","details":err})
+        }
+    }
+})
+
 //login a user to the system
 router.post("/login", async (req, res, next) => {
     try{
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,4 +12,21 @@ blogodb.one = (email) => {
     });
 }
 
+//create a new record
+blogodb.create = (userData) => {
+  return new Promise((resolve, reject)=>{
+    conn.query("SELECT Email FROM  admins WHERE Email = ?", [userData.email], (err, result) => {
+        if(err){return reject(err);}
+        if(result.length === 0){
+          conn.query('INSERT INTO admins(UserName, Email, Password, UserID, TimeStamp) VALUES( ?, ?, ?, ?, ? )', [ userData.username, userData.email, userData.password, userData.userID, userData.timestamp ], (err, results)=>{
+            if(err){return reject(err);}
+            return resolve(results)
+          });
+        }else{  
+          return reject({"error": "Email provided is already in use!", "message":"please login to your account", "details": err});
+        }
+    });
+  });
+}
+
 module.exports = blogodb
