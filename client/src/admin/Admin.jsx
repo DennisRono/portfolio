464919bd395f49ea7343438fb29249e30c6b05d2
DennisRono <finnneron1@gmail.com>
2023-01-18 18:44:01 +0300

    market compound egg happen frozen

--- client/src/admin/Admin.jsx
+++ client/src/admin/Admin.jsx
@@ -12,7 +12,7 @@ const Admin = () => {
     //verify jwt
     const checkLoggedIn = async (t, r) => {
       let res = await api('GET', 'verify', {token: t, refresh:r})
-      console.log(t, r);
+      console.log(res.data);
     }
     checkLoggedIn(getJwtToken(), getRefreshToken())
 
