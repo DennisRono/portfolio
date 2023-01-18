60d6c87f7f11b35f0d677ec79ea93f3614a06858
DennisRono <finnneron1@gmail.com>
2023-01-18 19:50:00 +0300

    letter refer measure prove any

--- client/src/admin/Admin.jsx
+++ client/src/admin/Admin.jsx
@@ -16,6 +16,7 @@ const Admin = () => {
         "refresh": r
       });      
       let res = await api('POST', 'verify', data)
+      console.log(res.data);
       if(res.data.type==='success'){
         setLoggedIn(true)
       }
