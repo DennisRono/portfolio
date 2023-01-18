624dbc838c14d0c1010e98818b13ef08db343da6
DennisRono <finnneron1@gmail.com>
2023-01-18 08:02:01 +0300

    wild simplest remain progress fair

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,9 +3,9 @@ const conn = require("../db/conn")
 let blogodb = {}
 
 //fetch one record
-blogodb.one = (email, userID) => {
+blogodb.one = (email) => {
     return new Promise((resolve, reject)=>{
-      conn.query('SELECT * FROM admins WHERE Email = ? || UserID = ?', [email, userID], (err, results)=>{
+      conn.query('SELECT * FROM admins WHERE Email = ?', [email], (err, results)=>{
         if(err){return reject(err);}
         return resolve(results[0])
       });
