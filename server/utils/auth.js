2205f4661f2663c792f7ce0e4c05d9127aab2f13
DennisRono <finnneron1@gmail.com>
2023-01-18 20:18:01 +0300

    effort wherever vertical floating growth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,9 +3,9 @@ const conn = require("../db/conn")
 let blogodb = {}
 
 //fetch one record
-blogodb.one = (email) => {
+blogodb.one = (email, userid) => {
     return new Promise((resolve, reject)=>{
-      conn.query('SELECT * FROM admins WHERE Email = ?', [email], (err, results)=>{
+      conn.query('SELECT * FROM admins WHERE Email = ? || UserID = ?', [email, userid], (err, results)=>{
         if(err){return reject(err);}
         return resolve(results[0])
       });
