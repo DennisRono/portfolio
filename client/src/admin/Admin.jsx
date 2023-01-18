eec37697112bc5647de8283d9c3e0e3267ff7b26
DennisRono <finnneron1@gmail.com>
2023-01-18 18:34:01 +0300

    wise world count kids cook

--- client/src/admin/Admin.jsx
+++ client/src/admin/Admin.jsx
@@ -2,11 +2,19 @@ import React, { Fragment, useEffect, useState } from 'react'
 import '../styles/css/admin.css'
 import { useNavigate } from "react-router-dom";
 import { getJwtToken, getRefreshToken } from '../includes/session'
+import { api } from '../api/axios'
 
 
 const Admin = () => {
     let navigate = useNavigate();
     let [LoggedIn, setLoggedIn] = useState(false)
+
+    //verify jwt
+    const checkLoggedIn = async (t, r) => {
+      let res = await api('GET', 'verify', {token: t, refresh:r})
+      console.log(res);
+    }
+    checkLoggedIn(getJwtToken(), getRefreshToken())
 
     useEffect(() => {
         if (!LoggedIn){
