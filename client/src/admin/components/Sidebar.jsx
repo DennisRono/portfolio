671349bd436a067633750cdd08eeb8135e26ec2a
DennisRono <finnneron1@gmail.com>
2023-01-20 14:18:01 +0300

    tea street kitchen daughter adult

--- client/src/admin/components/Sidebar.jsx
+++ client/src/admin/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
 import '../styles/css/sidebar.css'
-import Logo from '../../images/logo.png'
 import {ReactComponent as Home} from '../../assets/svg/home.svg'
 
 const Sidebar = () => {
@@ -8,7 +7,6 @@ const Sidebar = () => {
     <Fragment>
         <div className="adminSidebar">
           <div className="brand">
-            <img src={Logo} alt="" />
             <h1>Administrator</h1>
           </div>
           <div className="adminnavigation">
