76e6af7a0827732ebaf34fe5fb73afce2e96d9dc
DennisRono <finnneron1@gmail.com>
2023-01-20 14:16:01 +0300

    loss captured doctor television free

--- client/src/admin/components/Sidebar.jsx
+++ client/src/admin/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
+import '../styles/css/sidebar.css'
 import Logo from '../../images/logo.png'
+import {ReactComponent as Home} from '../../assets/svg/home.svg'
 
 const Sidebar = () => {
   return (
@@ -7,7 +9,15 @@ const Sidebar = () => {
         <div className="adminSidebar">
           <div className="brand">
             <img src={Logo} alt="" />
-            <h1>DennisKibet</h1>
+            <h1>Administrator</h1>
+          </div>
+          <div className="adminnavigation">
+            <ul>
+              <li>
+                  <Home className="navIcs" />
+                  <span>Dashboard</span>
+              </li>
+            </ul>
           </div>
         </div>
     </Fragment>
