697db565de727c9ce20276e9287e13a95c80a26f
DennisRono <finnneron1@gmail.com>
2023-01-20 14:04:01 +0300

    support show ability unhappy slowly

--- client/src/admin/components/Sidebar.jsx
+++ client/src/admin/components/Sidebar.jsx
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react'
 const Sidebar = () => {
   return (
     <Fragment>
-        
+        <div className="adminSidebar">
+          <h1>DennisKibet</h1>
+        </div>
     </Fragment>
   )
 }
