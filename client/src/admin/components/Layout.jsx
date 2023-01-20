4e207ddccd1ba46003092d5601b74357097d1e77
DennisRono <finnneron1@gmail.com>
2023-01-20 13:50:02 +0300

    same tales few sister plates

--- client/src/admin/components/Layout.jsx
+++ client/src/admin/components/Layout.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment } from 'react'
 import '../styles/css/layout.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
 
 const Layout = () => {
   return (
     <Fragment>
         <div className="layout">
-            <div className="adsidebar"></div>
+            <div className="adsidebar">
+                <Sidebar/>
+            </div>
             <div className="adminmain">
-                <div className="adminheader">header</div>
-                <div className="adplayarea"></div>
+                <div className="adminheader">
+                    <Header/>
+                </div>
+                <div className="adminplayarea"></div>
             </div>
         </div>
     </Fragment>
