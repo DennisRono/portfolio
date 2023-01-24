787bdce187321488ab2e97769fbc29987d6cbbb5
DennisRono <finnneron1@gmail.com>
2023-01-24 16:10:01 +0300

    appearance impossible listen select rate

--- client/src/admin/Routes/Home.jsx
+++ client/src/admin/Routes/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
             <div className="dashHWidgets"></div>
             <div className="dashHWidgets"></div>
             <div className="dashHWidgets"></div>
-            <div className="dashHWidgets"></div>
           </div>
         </div>
     </Fragment>
