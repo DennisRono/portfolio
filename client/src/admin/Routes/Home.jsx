704bea26178c25b32dcadbfcf66c2fec2a06f0b6
DennisRono <finnneron1@gmail.com>
2023-01-24 11:06:01 +0300

    temperature frame exchange door simple

--- client/src/admin/Routes/Home.jsx
+++ client/src/admin/Routes/Home.jsx
@@ -1,9 +1,21 @@
 import React, { Fragment } from 'react'
+import '../styles/css/home.css'
 
 const Home = () => {
   return (
     <Fragment>
-        
+        <div className="dashHome">
+          <div className="dashHomeContainer">
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+            <div className="dashHWidgets"></div>
+          </div>
+        </div>
     </Fragment>
   )
 }
