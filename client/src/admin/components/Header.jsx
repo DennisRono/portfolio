3722870857def79817af40695c318bed40805724
DennisRono <finnneron1@gmail.com>
2023-01-24 11:12:01 +0300

    double research bad solve gray

--- client/src/admin/components/Header.jsx
+++ client/src/admin/components/Header.jsx
@@ -3,7 +3,11 @@ import React, { Fragment } from 'react'
 const Header = () => {
   return (
     <Fragment>
-        
+        <div className="adminheader">
+          <div className="adminHContainer">
+            <h1>Dashboard</h1>
+          </div>
+        </div>
     </Fragment>
   )
 }
